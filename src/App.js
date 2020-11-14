c25d786c196f25a7531eda0806bc395d183440f3
Yassine <m.yassine.seltene@gmail.com>
2020-11-14 22:31:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import FullName from './Component/Profile/FullName.js';
-import Address from './Component/Profile/Address.js';
-import ProfilPhoto from './Component/Profile/ProfilPhoto.js';
+import Profile from './profile/Profile.js';
+import moi from "./profile/moi.jpg";
 import './App.css';
 
 function App() {
-  
+const alerte = (fullName) => {alert (fullName)} ;
   return (
     <div>
-     <FullName />;
-     <Address/>;
-     <ProfilPhoto />;
-    </div>
-  );
-}
+     <Profile fullName="Yassine Seltene" bio="Je suis un développeur Web fullstack js" profession="ingénieur développeur web" handleName={alerte}  /> 
+     <profile> <img src={moi} alt="Yassine Seltene"/> </profile>
 
+</div>
+  )
+}
 export default App;
